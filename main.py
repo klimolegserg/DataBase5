7d3fcf4ec56edcd4afddf9f61784ec0a60e9a38a
Oleg Klimenko <klimmail@rambler.ru>
2024-02-06 12:42:49 +0300

    change

---
+++ main.py
@@ -0,0 +1,113 @@
+import psycopg2
+
+
+def create_tab(conn):
+    with conn.cursor() as cur:
+
+        cur.execute("""
+        DROP TABLE phones;
+        DROP TABLE client;
+        """)
+
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS client(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(40),
+        last_name VARCHAR(40),
+        email VARCHAR(40)
+        );
+        """)
+
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS phones(
+        id SERIAL PRIMARY KEY,
+        client_id INTEGER REFERENCES client(id),
+        phone_number INTEGER
+        );
+        """)
+
+    conn.commit()
+    return
+
+
+def add_client(conn, name, last_name, email, phone_number=None):
+    with conn.cursor() as cur:
+        cur.execute("""
+        INSERT INTO client (name, last_name, email)
+        VALUES (%s, %s, %s)
+        RETURNING id, name, last_name, email;
+        """, (name, last_name, email))
+
+        conn.commit()
+
+
+def add_phone(conn, id, phone_number):
+    with conn.cursor() as cur:
+        cur.execute("""
+        INSERT INTO phones (id, phone_number)
+        VALUES (%s, %s)
+        RETURNING id, phone_number;
+        """, (id, phone_number))
+    return
+
+
+def change_client(conn, id, name=None, last_name=None, email=None, phone_number=None):
+    with conn.cursor() as cur:
+        cur.execute("""
+        UPDATE client SET name=%s, last_name=%s, email=%s
+        WHERE id=%s
+        RETURNING id, name, last_name, email;
+        """, (name, last_name, email, id))
+
+        cur.execute("""
+        UPDATE phones SET phone_number=%s
+        RETURNING id, phone_number;
+        """, (phone_number,))
+
+    return
+
+
+def delete_phone(conn, id, phone_number):
+    with conn.cursor() as cur:
+        cur.execute("""
+        DELETE FROM phones
+        WHERE id=%s AND phone_number=%s
+        RETURNING id;
+        """, (id,))
+    return
+
+
+def delete_client(conn, id):
+    with conn.cursor() as cur:
+        cur.execute("""
+        DELETE FROM client
+        WHERE id=%s;
+        """, (id,))
+    conn.commit()
+    return
+
+
+def find_client(conn, name=None, last_name=None, email=None, phone_number=None):
+    with conn.cursor() as cur:
+        cur.execute("""
+        SELECT * FROM client cl
+        JOIN phones ph ON cl.id = ph.id
+        WHERE (name = %(name)s OR %(name)s IS NULL)
+        AND (last_name = %(last_name)s OR %(last_name)s IS NULL)
+        AND (email = %(email)s OR %(email)s IS NULL)
+        AND (phone_number = %(phone)s OR %(phone)s IS NULL);
+        """, {'name': name, 'last_name': last_name, 'email': email, 'phone': phone_number})
+        
+    return
+
+
+with psycopg2.connect(database='database4', user='postgres', password='Python2023') as conn:
+    create_tab(conn)
+    add_client(conn, 'ivan', 'ivanov', 'iivanov@)gmail.com')
+    add_phone(conn, 1, 781299999)
+    change_client(conn, 1, 'Petr', 'Petrov', 'ppetrov@)gmail.com', 781288888)
+    delete_phone(conn, 1, 781288888)
+    delete_client(conn, 1)
+    find_client(conn, 'Petr', 'Petrov', 'ppetrov@gmail.com', 881288888)
+
+conn.close()
